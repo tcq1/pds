ab3f018642792066343a6b43078b9546d23449d7
tcq1 <chu.trung@web.de>
2020-12-12 18:40:34 +0100

    Implementation eclat algorithm

--- 07_association_rule_mining/07_quoc_trung_chu.py
+++ 07_association_rule_mining/07_quoc_trung_chu.py
@@ -4,8 +4,8 @@ import numpy as np
 from timeit import default_timer as timer
 
 
-def get_support(items, dataset):
-    """ Gets support of an item in an itemset.
+def get_support_apriori(items, dataset):
+    """ Gets support of an item in a dataset with a horizontal layout.
 
     :param items: set of items
     :param dataset: list of transactions
@@ -14,6 +14,22 @@ def get_support(items, dataset):
     support_set = [items.issubset(x) for x in dataset]
 
     return support_set.count(True) / len(dataset)
+
+
+def get_support_eclat(items, item_dict, dataset):
+    """ Gets support of an item in a dataset with a vertical layout.
+
+    :param items: set of items
+    :param item_dict: vertical layout
+    :param dataset: list of transactions
+    """
+    items = list(items)
+    transactions = item_dict[items[0]]
+
+    for item in items:
+        transactions = transactions.intersection(item_dict[item])
+
+    return len(transactions) / len(dataset)
 
 
 def get_subsets(itemset, length):
@@ -77,30 +93,75 @@ def apriori_gen(k_minus_one_itemsets):
 
 
 def apriori(items, dataset, minsupp):
-    """ Implementation of a priori algorithm.
+    """ Implementation of the a priori algorithm.
 
     :param items: list of items
     :param dataset: np array with transactions/itemsets
     :param minsupp: min support
-    :return: association rules
+    :return: frequent item sets
     """
     # note: 1-itemsets are in Lk[0], 2-itemsets are in Lk[1], etc.
-    print('Getting 1-itemsets...')
-    start = timer()
-    Lk = [set(frozenset([item]) for item in items if get_support({item}, dataset) >= minsupp)]
-    end = timer()
-    print('This took {}s!'.format(end - start))
+    Lk = [set(frozenset([item]) for item in items if get_support_apriori({item}, dataset) >= minsupp)]
     k = 1
 
     # while we can still generate frequent itemsets
     while len(Lk[k-1]) > 0:
-        print('Getting {}-itemsets...'.format(k+1))
         # get candidates for k-itemset from k-1 itemset
         Ck = apriori_gen(Lk[k-1])
         fis_k = set()
         # pruning to get frequent itemsets from Ck
         for candidate in Ck:
-            if get_support(candidate, dataset) >= minsupp:
+            if get_support_apriori(candidate, dataset) >= minsupp:
+                fis_k.add(candidate)
+        # add frequent k-itemset to Lk
+        Lk.append(fis_k)
+        k += 1
+
+    return set.union(*Lk)
+
+
+def get_vertical_layout(items, dataset):
+    """ Make the initially horizontal layout vertical
+
+    :param items: list of items
+    :param dataset: np array with all transactions
+    """
+    item_dict = {}
+    for item in items:
+        transactions = set()
+        for i in range(len(dataset)):
+            if item in dataset[i]:
+                transactions.add(i)
+        item_dict[item] = transactions
+
+    return item_dict
+
+
+def eclat(items, dataset, minsupp):
+    """ Implementation of the eclat algorithm.
+
+    :param items: list of items
+    :param dataset: np array with transactions
+    :param minsupp: min support
+    :return: frequent item sets
+    """
+    item_dict = get_vertical_layout(items, dataset)
+    to_remove = []
+    for item in item_dict.keys():
+        if get_support_eclat([item], item_dict, dataset) < minsupp:
+            to_remove.append(item)
+    for tr in to_remove:
+        item_dict.pop(tr)
+
+    Lk = [set(frozenset([item]) for item in item_dict.keys())]
+    k = 1
+
+    while len(Lk[k-1]) > 0:
+        Ck = apriori_gen(Lk[k - 1])
+        fis_k = set()
+        # pruning to get frequent itemsets from Ck
+        for candidate in Ck:
+            if get_support_eclat(candidate, item_dict, dataset) >= minsupp:
                 fis_k.add(candidate)
         # add frequent k-itemset to Lk
         Lk.append(fis_k)
@@ -129,25 +190,45 @@ def read_file(file_path):
     return dataset
 
 
-def main():
-    # file_path = 'items.tsv'
-    # minsupp = 0.7
+def run_apriori(items, dataset, minsupp):
+    print('Starting A PRIORI algorithm...')
+    start = timer()
+    fis = apriori(items, dataset, minsupp=minsupp)
+    end = timer()
+    # for f in fis:
+    #     print('{}'.format(f))
+    print('Found {} frequent itemsets'.format(len(fis)))
+    print('Done after {}s!'.format(end - start))
 
+
+def run_eclat(items, dataset, minsupp):
+    print('Starting ECLAT algorithm...')
+    start = timer()
+    fis = eclat(items, dataset, minsupp=minsupp)
+    end = timer()
+    # for f in fis:
+    #     print('{}'.format(f))
+    print('Found {} frequent itemsets'.format(len(fis)))
+    print('Done after {}s!'.format(end - start))
+
+
+def main():
     file_path = 'retail.tsv'
     minsupp = 0.1
+    # results: ECLAT: 257.1290893s
+    #       A PRIORI: 610.4664944000001s
+
+    # file_path = 'items.tsv'
+    # minsupp = 0.7
+    # results: ECLAT: 0.0005955000000000127s
+    #       A PRIORI: 0.0009656999999999999s
 
     dataset = read_file(file_path)
     items = np.unique([item for sublist in dataset for item in sublist])
     items = items[items != '']
 
-    print('Starting')
-    start = timer()
-    fis = apriori(items, dataset, minsupp=minsupp)
-    end = timer()
-    for f in fis:
-        print('{}'.format(f))
-    print('Found {} frequent itemsets'.format(len(fis)))
-    print('Done after {}s!'.format(end - start))
+    run_eclat(items, dataset, minsupp)
+    run_apriori(items, dataset, minsupp)
 
 
 if __name__ == '__main__':
