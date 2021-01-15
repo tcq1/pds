bb8531813f20e9838c696cb95f7f359c27b36331
tcq1 <chu.trung@web.de>
2021-01-15 15:24:14 +0100

    final version

--- 09_forecasting/09_quoc_trung_chu.py
+++ 09_forecasting/09_quoc_trung_chu.py
@@ -149,7 +149,7 @@ def main():
         start_shop = timer()
         best = grid_search(shop['transactions'], 3, 3)
         print("Done with shop {} after {}s.".format(shop.iloc[0]['shop_id'], timer() - start_shop))
-        print("p: {}, q: {}, MSE: {}, parameters: {}".format(best[0][0], best[0][1], best[1].fun, best[1].x))
+        print("p: {}, q: {}, MSE: {}, parameters: {}\n".format(best[0][0], best[0][1], best[1].fun, best[1].x))
 
     print("Done with all time series after {}s.".format(timer() - start_total))
 
@@ -157,28 +157,38 @@ def main():
     
     Done with shop 145 after 28.5056102s.
     p: 2, q: 2, MSE: 215.53414485111696, parameters: [ 1.57794589 -0.58052086  0.11120508 -1.19728612  0.10304867]
+    
     Done with shop 260 after 26.602319599999998s.
     p: 1, q: 2, MSE: 373.56839408712653, parameters: [ 0.68711732 26.18318107 -0.44103851  0.04996526]
+    
     Done with shop 315 after 23.080452199999996s.
     p: 2, q: 2, MSE: 323.6651118211104, parameters: [ 0.15121227  0.8587056  -0.59962977  0.24356502 -0.74502043]
+    
     Done with shop 375 after 28.21356s.
     p: 1, q: 3, MSE: 229.71010897085569, parameters: [ 0.94607106  2.50898613 -0.7200216  -0.07855872 -0.38468647]
+    
     Done with shop 548 after 30.800740900000008s.
     p: 3, q: 3, MSE: 335.3657714992873, parameters: [-0.00517792  0.15808891  0.83531331  0.66043892  0.03498125 -0.25753891
      -1.09691392]
+     
     Done with shop 560 after 27.316482400000012s.
     p: 3, q: 3, MSE: 1076.1151601533625, parameters: [ 0.85036988  0.42865143 -0.27594663 -0.62356542 -0.51650497 -0.8367294
       0.183589  ]
+      
     Done with shop 750 after 25.378438700000004s.
     p: 3, q: 3, MSE: 567.1980406143393, parameters: [ 1.13717765 -0.41880113  0.27862219  0.61953468 -1.02526741  0.39340039
      -0.22829494]
+     
     Done with shop 897 after 27.76999459999999s.
     p: 3, q: 3, MSE: 12173.37622525072, parameters: [ 0.76842068 -0.58223577  0.79557697 -0.32767416 -0.53719915  0.08873952
      -0.7993756 ]
+     
     Done with shop 1332 after 28.491905599999996s.
     p: 2, q: 3, MSE: 3299.616715119028, parameters: [ 1.2264627  -0.23596949  0.61406472 -0.80299952 -0.40896393  0.06080798]
+    
     Done with shop 1750 after 23.425931100000014s.
     p: 2, q: 2, MSE: 429.40212750275066, parameters: [ 1.50992107 -0.50376428 -0.32331789 -1.06310663 -0.06116204]
+    
     Done with all time series after 269.5872852s.
     """
 
