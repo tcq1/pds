6fbf4bff59bface706f87d7e2c29da7845c76a40
tcq1 <chu.trung@web.de>
2021-01-28 12:31:36 +0100

    add another better model

--- 12_neural_networks/12_quoc_trung_chu.py
+++ 12_neural_networks/12_quoc_trung_chu.py
@@ -5,6 +5,8 @@ from keras import Sequential
 from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 from keras.utils import to_categorical
 
+from timeit import default_timer as timer
+
 
 def get_model(input_shape):
     """ Get the untrained CNN model.
@@ -19,6 +21,28 @@ def get_model(input_shape):
         Flatten(),                                                                         # flatten matrix input
         Dense(32, activation='relu'),                                                      # dense layer
         Dense(10, activation='softmax')                                                    # output layer
+    ])
+    model.compile(optimizer='adam', loss=keras.losses.categorical_crossentropy, metrics=['accuracy'])
+
+    return model
+
+
+def test_model(input_shape):
+    """ Get an untrained CNN model. This model has some different hyper parameters.
+
+    :param input_shape: shape of input data
+    :return: keras Sequential model
+    """
+    model = Sequential([
+        Conv2D(filters=32, kernel_size=3, input_shape=input_shape, activation='relu'),
+        Conv2D(filters=32, kernel_size=3, activation='relu'),
+        Conv2D(filters=32, kernel_size=5, activation='relu', strides=2),
+        Conv2D(filters=64, kernel_size=3, activation='relu'),
+        Conv2D(filters=64, kernel_size=3, activation='relu'),
+        Conv2D(filters=64, kernel_size=5, activation='relu', strides=2),
+        Flatten(),
+        Dense(128, activation='relu'),
+        Dense(10, activation='softmax')
     ])
     model.compile(optimizer='adam', loss=keras.losses.categorical_crossentropy, metrics=['accuracy'])
 
@@ -52,11 +76,33 @@ def main():
     # get training and validation data
     (x_train, y_train), (x_validate, y_validate) = get_data()
 
-    # get, train and evaluate model
+    # get and train the first model
+    start1 = timer()
     model = get_model(x_train[0].shape)
     model.fit(x_train, y_train, epochs=12, batch_size=32, shuffle=True)
+    end1 = timer()
+
+    # get and train the test model
+    start2 = timer()
+    test = test_model(x_train[0].shape)
+    test.fit(x_train, y_train, epochs=12, batch_size=32, shuffle=True)
+    end2 = timer()
+
     print("\n----------------- Model evaluation -----------------")
+    print("First model: ")
     model.evaluate(x_validate, y_validate)
+    print("Second model: ")
+    test.evaluate(x_validate, y_validate)
+
+    print("Training of first model done after {}s.".format(end1 - start1))
+    print("Training of second model done after {}s.".format(end2 - start2))
+
+    """
+    First model: loss: 0.0607 - accuracy: 0.9880
+    Second model: loss: 0.0341 - accuracy: 0.9923
+    Training of first model done after 475.1620607s.
+    Training of second model done after 587.2978638s.
+    """
 
 
 if __name__ == '__main__':
